e398cbb7e4c8d3abc06a4e560c73f0382e016379
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2023-06-22 13:58:08 +0200

    minor

--- src/main/java/com/mytests/spring/sbMapConfigProps/TestController.java
+++ src/main/java/com/mytests/spring/sbMapConfigProps/TestController.java
@@ -3,7 +3,6 @@ package com.mytests.spring.sbMapConfigProps;
 import com.mytests.spring.sbMapConfigProps.config.DemoProperties;
 import com.mytests.spring.sbMapConfigProps.config.MoreDemoProperties;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
